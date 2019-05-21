803d0b15ff1f5045806c653c319b98b58d4a307f
wescottsharples <wesc@ttsharpl.es>
2019-05-21 22:39:47 +0000

    Verify kwargs and update styling

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+Contains Square class
+"""
 
-class square():
+
+class Square():
+    """
+    Defines Square class
+    """
     
     width = 0
     height = 0
@@ -9,12 +16,16 @@ class square():
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
+        if self.width and self.width != self.height:
+            self.height = self.width
+        elif self.width != self.height:
+            self.width = self.height
 
-    def area_of_my_square(self):
+    def area(self):
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def perim(self):
         return (self.width * 2) + (self.width * 2)
 
     def __str__(self):
@@ -24,5 +35,5 @@ if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.area())
+    print(s.perim())
